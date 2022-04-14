160b34b8b9d2627c7a8be4309011d2cdf81edf8f
sesorov <samws@mail.ru>
2022-04-14 17:41:48 +0300

    Huge msg handler refactor, add bandit workflow

--- infrastructure/keyboards/inline_keyboards.py
+++ infrastructure/keyboards/inline_keyboards.py
@@ -7,6 +7,22 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from infrastructure.keyboards.callbacks import *
 
 
+async def get_custom_keyboard(buttons: list) -> InlineKeyboardMarkup:
+    """
+    Create custom keyboard with dynamic buttons
+    Receives list of lists of dicts, where each list represents a row of buttons (dicts - name and callback)
+
+    :param buttons: List of lists of dicts, e.g.: [[{'row_1_1': 11}, {...}], [{'row_2_1': 21}, {...}]]
+    :return:
+    """
+
+    keyboard = InlineKeyboardMarkup()
+    for row in buttons:
+        keyboard.add(*[InlineKeyboardButton(list(button.items())[0][0], callback_data=list(button.items())[0][1])
+                       for button in row])
+    return keyboard
+
+
 async def get_help_keyboard() -> InlineKeyboardMarkup:
     """
     Chat help keyboard
@@ -59,5 +75,17 @@ async def get_ask_email_keyboard() -> InlineKeyboardMarkup:
     keyboard.add(
         InlineKeyboardButton("Yes", callback_data=CALLBACK_EMAIL_TRUE),
         InlineKeyboardButton("No", callback_data=CALLBACK_EMAIL_FALSE),
+    )
+    return keyboard
+
+
+async def get_teacher_group_actions_keyboard() -> InlineKeyboardMarkup:
+    """
+    View available TEACHER's actions in selected group
+    """
+
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(
+        InlineKeyboardButton("Create task", callback_data=CALLBACK_CREATE_TASK)
     )
     return keyboard
